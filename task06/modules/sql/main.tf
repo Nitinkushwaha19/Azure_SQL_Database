365888122744f7ce8ed4a8e989e0e002253bdcfa
nitinkushwaha <nitinkushwaha706@outlook.com>
2026-04-20 11:57:00 +0530

    fixed issues

--- task06/modules/sql/main.tf
+++ task06/modules/sql/main.tf
@@ -63,5 +63,6 @@ resource "azurerm_key_vault_secret" "sql_admin_password" {
   value        = random_password.sql_admin_password.result
   key_vault_id = var.key_vault_id
 
-  tags = var.tags
+  depends_on = [random_password.sql_admin_password]
+  tags       = var.tags
 }
